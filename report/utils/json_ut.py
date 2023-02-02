22a2180939cdb771f772037f22f61242266de3bb
Ramit Suri <ramitsuri007@gmail.com>
2023-02-02 09:40:45 -0500

    Add report name to json

--- report/utils/json_ut.py
+++ report/utils/json_ut.py
@@ -4,9 +4,16 @@ from decimal import Decimal
 
 def print_json(path, report_name, totals_for_accounts):
     jsonpickle.handlers.registry.register(Decimal, _DecimalHandler)
-    json_string = jsonpickle.encode(totals_for_accounts, unpicklable=False)
+    report = _Report(report_name, totals_for_accounts)
+    json_string = jsonpickle.encode(report, unpicklable=False)
     with open(path + report_name + '.json', 'w') as file:
         file.writelines(json_string)
+
+
+class _Report:
+    def __init__(self, name, account_totals):
+        self.name = name
+        self.account_totals = account_totals
 
 
 class _DecimalHandler(jsonpickle.handlers.BaseHandler):
