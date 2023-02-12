2c953665ab79b62b5e7593a4d033e0526a685038
Ramit Suri <ramitsuri007@gmail.com>
2023-02-12 14:07:07 -0500

    Add report generation time to json

--- report/utils/json_ut.py
+++ report/utils/json_ut.py
@@ -1,18 +1,21 @@
 import jsonpickle
 from decimal import Decimal
+from datetime import datetime
 
 
 def print_json(path, report_name, totals_for_accounts):
     jsonpickle.handlers.registry.register(Decimal, _DecimalHandler)
-    report = _Report(report_name, totals_for_accounts)
+    time = datetime.now().isoformat()
+    report = _Report(report_name, time, totals_for_accounts)
     json_string = jsonpickle.encode(report, unpicklable=False)
     with open(path + report_name + '.json', 'w') as file:
         file.writelines(json_string)
 
 
 class _Report:
-    def __init__(self, name, account_totals):
+    def __init__(self, name, time, account_totals):
         self.name = name
+        self.time = time
         self.account_totals = account_totals
 
 
