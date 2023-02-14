d8f1b7e76f7f553cecaa5bb35ad55be09efed940
Ramit Suri <ramitsuri007@gmail.com>
2023-02-14 05:52:04 -0500

    Move expense config to expense property in json

--- report/main.py
+++ report/main.py
@@ -15,15 +15,15 @@ def read_config():
 def main():
     config = read_config()
     gnu_cash_file = config['file']
-    years = config['years']
     time_delta = to_time_delta(config['time_delta_hours'])
 
     book = piecash.open_book(gnu_cash_file, readonly=True, open_if_lock=True)
 
-    exclude_parents = config['exclude_parents']
-    exclude_children = config['exclude_children']
-
     # Expense reports
+    expense_config = config['expense']
+    years = expense_config['years']
+    exclude_parents = expense_config['exclude_parents']
+    exclude_children = expense_config['exclude_children']
     expense.print_reports(book.accounts, years, time_delta, exclude_parents, exclude_children)
 
     book.close()
