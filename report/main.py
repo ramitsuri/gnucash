a67238e763fe58f6baa593722200fb6488ee5442
Ramit Suri <ramitsuri007@gmail.com>
2023-02-19 14:13:11 -0500

    Add config for which reports get printed

--- report/main.py
+++ report/main.py
@@ -20,27 +20,36 @@ def main():
     gnu_cash_file = config['file']
     time_delta = to_time_delta(config['time_delta_hours'])
 
+    print_expense = config['print_expense']
+    print_income = config['print_income']
+    print_assets = config['print_assets']
+    print_liabilities = config['print_liabilities']
+
     book = piecash.open_book(gnu_cash_file, readonly=True, open_if_lock=True)
 
     # Expense reports
-    expense_config = config['expense']
-    expense_root_account = book.root_account.children(name="Expenses")
-    expense.print_reports(expense_root_account, expense_config, time_delta)
+    if print_expense:
+        expense_config = config['expense']
+        expense_root_account = book.root_account.children(name="Expenses")
+        expense.print_reports(expense_root_account, expense_config, time_delta)
 
     # Income reports
-    income_config = config['income']
-    income_root_account = book.root_account.children(name="Income")
-    income.print_reports(income_root_account, income_config, time_delta)
+    if print_income:
+        income_config = config['income']
+        income_root_account = book.root_account.children(name="Income")
+        income.print_reports(income_root_account, income_config, time_delta)
 
     # Assets reports
-    assets_config = config['assets']
-    assets_root_account = book.root_account.children(name="Assets")
-    assets.print_reports(assets_root_account, assets_config, time_delta)
+    if print_assets:
+        assets_config = config['assets']
+        assets_root_account = book.root_account.children(name="Assets")
+        assets.print_reports(assets_root_account, assets_config, time_delta)
 
     # Liabilities reports
-    liabilities_config = config['liabilities']
-    liabilities_root_account = book.root_account.children(name="Liabilities")
-    liabilities.print_reports(liabilities_root_account, liabilities_config, time_delta)
+    if print_liabilities:
+        liabilities_config = config['liabilities']
+        liabilities_root_account = book.root_account.children(name="Liabilities")
+        liabilities.print_reports(liabilities_root_account, liabilities_config, time_delta)
 
     book.close()
 
